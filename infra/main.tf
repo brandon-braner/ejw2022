f6b1dba8aa60a89f7c47d6772ee7b11976a84dea
Brandon Braner <brandon.braner@gmail.com>
2022-02-07 00:06:04 -0600

    updated to 1024 mb

--- infra/main.tf
+++ infra/main.tf
@@ -92,7 +92,7 @@ resource "google_cloudfunctions_function" "function" {
   description = "My function"
   runtime     = "python39"
 
-  available_memory_mb   = 256
+  available_memory_mb   = 1024
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.zip.name
   trigger_http          = true

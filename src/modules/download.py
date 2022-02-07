1beae10fdc3f1b5c248040c7619f9c44fb194c73
Brandon Braner <brandon.braner@gmail.com>
2022-02-06 23:34:39 -0600

    switched download location to tmp for cloud function

--- src/modules/download.py
+++ src/modules/download.py
@@ -14,20 +14,21 @@ FILENAME = "INMT4AA1.zip"
 
 def download_file():
     # Download the zip file
+    download_file_path = f"/tmp/{FILENAME}"
     print("Downloading the zip file...")
     start_time = time.time()
     request.urlretrieve(
-        f"https://www.doc.state.nc.us/offenders/{FILENAME}", filename=f"/tmp/{FILENAME}"
+        f"https://www.doc.state.nc.us/offenders/{FILENAME}", filename=download_file_path
     )
     end_time = time.time()
     print(f"Downloaded in {end_time - start_time} seconds")
 
     # Check if file was successfully downloaded
-    if not os.path.exists(FILENAME):
+    if not os.path.exists(download_file_path):
         raise FileNotFoundError("Error in downloading the zip file.")
 
     # Extract the data from the zip file
-    with zipfile.ZipFile(FILENAME, "r") as zip_ref:
+    with zipfile.ZipFile(download_file_path, "r") as zip_ref:
         zip_ref.extractall("./")
 
     # Get last Saturday's date
@@ -35,7 +36,7 @@ def download_file():
     date = date.strftime("%m-%d-%Y")
 
     # Upload the files to the bucket
-    metafile = FILENAME.replace(".zip", ".des")
+    metafile = download_file_path.replace(".zip", ".des")
     # Check if file was successfully downloaded
     if not os.path.exists(metafile):
         raise FileNotFoundError(
@@ -46,7 +47,7 @@ def download_file():
     fs.put_file(metafile, f"{RAW_PATH}/{date}/{metafile}")
     end_time = time.time()
     print(f"Uploaded metafile in {end_time - start_time} seconds")
-    datafile = FILENAME.replace(".zip", ".dat")
+    datafile = download_file_path.replace(".zip", ".dat")
     # Check if file was successfully downloaded
     if not os.path.exists(datafile):
         raise FileNotFoundError(
